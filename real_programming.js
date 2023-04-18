53705bc685cb710e958827d34c42db0ce5608466
A7madAdelll <ahmadaa145632@gmail.com>
2023-04-18 10:13:13 +0200

    add player move

--- real_programming.js
+++ real_programming.js
@@ -1,34 +1,38 @@
-// function fun(){
-//     var x=document.getElementById("hiddenlist");
-// x.classList="actionlist";
-
-// }
-// function closefun(){
-//     var x=document.getElementById("hiddenlist");
-// x.classList="actionlist hidden";
-
-// }
-
-
-// action.addEventListener('click',(event) =>{
-// console.log(event);
-//     list.classList.toggle("hidden")
-
-// })
 
 const app={
  map: {
-    action : document.querySelector(".act"),
-    list : document.querySelector(".actionlist"),
+    action : document.querySelector('[data-id="actions"]'),
+    list : document.querySelector('[data-id="action-list"]'),
+    rest: document.querySelector('[data-id="reset"]'),
+    new_round: document.querySelector('[data-id="new-round"]'),
+    boxes : document.querySelectorAll('[data-id="box"]'),
 },
 init(){
-app.map.action.addEventListener("click",() =>{
-app.map.list.classList.toggle("hidden")
-})
+this.eventleseners();
+},
 
+eventleseners(){
+    app.map.action.addEventListener("click",() =>{
+        app.map.list.classList.toggle("hidden")
+        }),
 
+        app.map.rest.addEventListener("click",()=>{
+    console.log("rest");
+    app.map.list.classList.toggle("hidden")
+        }),
+
+    app.map.new_round.addEventListener("click",()=>{
+        console.log("new-round");
+        app.map.list.classList.toggle("hidden")
+        }),
+
+       app.map.boxes.forEach(element => {
+            element.addEventListener("click",()=>{
+                console.log(element.id);
+                element.textContent='x';
+            })
+        });
 }
-
 
 }
 window.addEventListener("load",() => app.init() )
