5bd4605087f76c9cb72c867ffc537f20f1c1d04b
A7madAdelll <ahmadaa145632@gmail.com>
2023-04-19 09:57:49 +0200

    now the players can take moves

--- real_programming.js
+++ real_programming.js
@@ -1,4 +1,6 @@
 
+const nine=[0,0,0,0,0,0,0,0,0,0]
+let turn=0;
 const app={
  map: {
     action : document.querySelector('[data-id="actions"]'),
@@ -6,6 +8,7 @@ const app={
     rest: document.querySelector('[data-id="reset"]'),
     new_round: document.querySelector('[data-id="new-round"]'),
     boxes : document.querySelectorAll('[data-id="box"]'),
+    topleft:document.querySelector('[data-id="topleft"]'),
 },
 init(){
 this.eventleseners();
@@ -27,9 +30,70 @@ eventleseners(){
         }),
 
        app.map.boxes.forEach(element => {
-            element.addEventListener("click",()=>{
-                console.log(element.id);
-                element.textContent='x';
+            element.addEventListener("click",(event)=>{
+
+if(turn==0 && nine[element.id]==0){
+    const iconx= document.createElement("i");
+    iconx.classList.add('fa-solid' ,'fa-x' ,'clearbackground');
+    const icony= document.createElement("i");
+    icony.classList.add('fa-solid' ,'fa-o','clearbackground');
+
+    
+    const icondiv=document.createElement("div");
+    icondiv.classList.add('topleftdiv')
+    icondiv.appendChild(icony);
+    const p=document.createElement("p");
+    p.classList.add('topleftp')
+    p.innerHTML="player 2 has to play";
+
+   
+    
+    app.map.topleft.replaceChildren();
+    app.map.topleft.appendChild(icondiv);
+    app.map.topleft.appendChild(p);
+
+   
+    console.log(element.id);
+    element.replaceChildren(iconx);
+    nine[element.id]=1
+    turn=1;
+}
+else if(turn==1 && nine[element.id]==0){
+
+    const icony= document.createElement("i");
+    icony.classList.add('fa-solid' ,'fa-o','clearbackground');
+    const iconx= document.createElement("i");
+    iconx.classList.add('fa-solid' ,'fa-x' ,'clearbackground');
+
+    const icondiv=document.createElement("div");
+    icondiv.classList.add('topleftdiv')
+    icondiv.appendChild(iconx);
+    const p=document.createElement("p");
+    p.classList.add('topleftp')
+    p.innerHTML="player 1 has to play";
+
+   
+    
+    app.map.topleft.replaceChildren();
+    app.map.topleft.appendChild(icondiv);
+    app.map.topleft.appendChild(p);
+
+
+    console.log(element.id);
+    element.replaceChildren(icony);
+    
+     nine[element.id]=2;
+    turn=0;
+}
+else{
+    console.log("a7aa");
+
+}
+
+               
+               
+               
+
             })
         });
 }
